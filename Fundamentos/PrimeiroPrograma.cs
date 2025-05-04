bbcd7a7063ffb42990733c54ef1bd9e131a0bf1b
Breno Carvalho <carv.breno.dev@gmail.com>
2025-05-04 15:56:53 -0300

    first commit with first exercises

---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,8 @@
+namespace CursoCSharp.Fundamentos {
+    public class PrimeiroPrograma {
+        public static void Executar() {
+            Console.WriteLine("Primeiro Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
